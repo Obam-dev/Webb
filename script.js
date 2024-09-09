f44f84c1e8c0d16a3de110a0eed09f0695932f3a
Obam-dev <botku109@gmail.com>
2024-09-09 17:35:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 document.getElementById('whatsappForm').addEventListener('submit', function(event) {
     event.preventDefault();  // Mencegah form reload halaman
     
-    var phone = document.getElementById('phone').value;
-    var message = document.getElementById('message').value;
+    var phone = document.getElementById('phone').value.trim();
+    var message = document.getElementById('message').value.trim();
     
-    // Mengecek apakah nomor telepon dimulai dengan 62
-    if (phone.startsWith('62')) {
+    // Validasi nomor, cek apakah dimulai dengan 62 dan memiliki panjang minimal 10 karakter
+    if (phone.startsWith('62') && phone.length >= 10) {
+        // Buat URL WhatsApp
         var whatsappUrl = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
-        window.open(whatsappUrl, '_blank');  // Membuka WhatsApp di tab baru
+        
+        // Buka WhatsApp di tab baru
+        window.open(whatsappUrl, '_blank');
     } else {
         alert('Nomor WhatsApp harus dalam format internasional dan dimulai dengan 62.');
     }
