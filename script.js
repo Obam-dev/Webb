0e7a8a2207b9332719f2a6f9529195a4d9e3a133
Obam-dev <botku109@gmail.com>
2024-09-10 06:00:16 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-document.getElementById('dataDiriForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    // Ambil data dari form
-    var nama = document.getElementById('nama').value;
-    var kelas = document.getElementById('kelas').value;
-    var umur = document.getElementById('umur').value;
-
-    // Nomor WhatsApp tujuan
-    var nomorWhatsApp = '6285757572590';
-
-    // Pesan yang akan dikirim
-    var pesan = `Halo, ini data diri saya:%0A%0ANama: ${nama}%0AKelas: ${kelas}%0AUmur: ${umur}`;
-
-    // Buat URL WhatsApp
-    var url = `https://api.whatsapp.com/send?phone=${nomorWhatsApp}&text=${pesan}`;
-
-    // Redirect ke WhatsApp
-    window.open(url, '_blank');
-});
+//reset animations
+setInterval(()=>{
+  let el = document.getElementById('love')
+  var newone = el.cloneNode(true);
+  el.parentNode.replaceChild(newone, el);
+}, 4000)
