3135b14debe381ff42f449d94fa5386f453288ff
Obam-dev <botku109@gmail.com>
2024-09-09 18:05:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 document.getElementById('playAudio').addEventListener('click', function() {
     var audio = document.getElementById('audioPlayer');
+    
+    // Pastikan audio dimuat sebelum diputar
+    audio.load();  
     audio.play();  // Memainkan audio
 
-    // Setelah audio selesai dimainkan, teks ditampilkan secara berurutan dengan delay
+    // Setelah audio mulai diputar, tampilkan teks secara berurutan dengan delay
     setTimeout(function() {
         document.getElementById('text1').style.display = 'block';
     }, 1000); // Tampilkan teks 1 setelah 1 detik
 
     setTimeout(function() {
         document.getElementById('text2').style.display = 'block';
-    }, 5000); // Tampilkan teks 2 setelah 5 detik
+    }, 20000); // Tampilkan teks 2 setelah 20 detik
 
     setTimeout(function() {
         document.getElementById('text3').style.display = 'block';
-    }, 10000); // Tampilkan teks 3 setelah 10 detik
+    }, 25000); // Tampilkan teks 3 setelah 25 detik
 });
